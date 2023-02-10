c3219c230f368a862a592c7e660942aa007f6c32
Shubham Patil <30755507+spshubham@users.noreply.github.com>
2023-02-10 10:43:59 +0530

    Update conf.js

--- conf/conf.js
+++ conf/conf.js
@@ -2,8 +2,8 @@ exports.JWT_SECRET_KEY = "sp_jwt_secret_key"
   
 exports.TOKEN_HEADER_KEY = "sp_token_header_key"
 
-exports.NEWS_API_KEY = "de5926d50489471e8ddee7de301ddbca"
+exports.NEWS_API_KEY = ""
 
-exports.FORECAST_API_KEY = "fddd65b52d5a5550720794892b502bc9"
+exports.FORECAST_API_KEY = ""
 
 exports.TOKEN_VALID_TIME = "5h"
